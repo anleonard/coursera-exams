b8073ba11b317388b3b38efdbcb202ad7c9e10f1
anleonard <andrealeonardo.nardi@gmail.com>
2017-05-09 17:56:48 +0200

    Fist code for module1 solution

--- module1-solution/app.js
+++ module1-solution/app.js
@@ -1,11 +1,40 @@
 (function () {
 'use strict';
 
-  angular.module('LunchCheck', [])
+angular.module('LunchCheck', [])
   .controller('LunchCheckController', LunchCheckController);
-  LunchCheckController.$inject = ['$scope'];
+
   function LunchCheckController($scope) {
 
+    var $lista = [];
+
+    $scope.printMessage = function () {
+
+        var numElementi = 0;
+        var $listaElementi = $scope.lista;
+
+        if($listaElementi !=null && $listaElementi.length>0){
+
+          $scope.arrayElementi = [];
+          var splitLista = $listaElementi.split(',');
+
+          angular.forEach(splitLista, function(value, key) {
+            if(value.length>0) $scope.arrayElementi.push(value);
+          });
+
+          numElementi = $scope.arrayElementi.length;
+        }
+
+        $scope.messageStyle ="red-font";
+        $scope.inputStyle = "valid-error";
+        if(numElementi==0) $scope.messaggio = "Please enter data first";
+        else{
+          $scope.messageStyle ="green-font";
+          $scope.inputStyle = "valid-success";
+          if(numElementi<=3) $scope.messaggio = "Enjoy";
+          else $scope.messaggio = "TooMuch!";
+        }
+    };
   }
 
-})
+})();
