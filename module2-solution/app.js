ecacc2a654a538e3eacade0bcb8286b0cafc53f1
anleonard <andrealeonardo.nardi@gmail.com>
2017-05-25 15:41:45 +0200

    New commit

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -6,43 +6,35 @@ angular.module('ShoppingListApp', [])
 .controller('AlreadyBoughtController', AlreadyBoughtController)
 .service('ShoppingListCheckOff', ShoppingListCheckOff);
 
+//ToBuyController
 ToBuyController.$inject = ['ShoppingListCheckOff'];
 function ToBuyController(ShoppingListCheckOff) {
   var itemAdder = this;
   itemAdder.items = ShoppingListCheckOff.getItems();
-  itemAdder.addItem = function (itemName, itemQuantity) {
+  itemAdder.addItem = function (itemName, itemQuantity, itemIndex) {
     ShoppingListCheckOff.addItem(itemName, itemQuantity);
+    ShoppingListCheckOff.removeToBuy(itemIndex);
   }
 }
 
+//AlreadyBoughtController
 AlreadyBoughtController.$inject = ['ShoppingListCheckOff'];
 function AlreadyBoughtController(ShoppingListCheckOff) {
   var boughtManager = this;
   boughtManager.boughtItems = ShoppingListCheckOff.getBoughtItems();
 }
 
+//ShoppingListCheckOff service
 function ShoppingListCheckOff() {
   var service = this;
   var boughtItems = [];
 
-  // List of shopping items
   var items = [
-    {
-      name: "Milk",
-      quantity: "2"
-    },
-    {
-      name: "Donuts",
-      quantity: "200"
-    },
-    {
-      name: "Cookies",
-      quantity: "300"
-    },
-    {
-      name: "Chocolate",
-      quantity: "5"
-    }
+    { name: "Candies", quantity: "78" },
+    { name: "Biscuits", quantity: "20" },
+    { name: "Cookies", quantity: "32" },
+    { name: "Chocolate", quantity: "5" },
+    { name: "Bread", quantity: "8" }
   ];
 
   service.addItem = function (itemName, quantity) {
@@ -64,6 +56,15 @@ function ShoppingListCheckOff() {
   service.getBoughtItems = function() {
     return boughtItems;
   }
+
+  service.getItemsSize = function () {
+    return items.size();
+  }
+
+  service.removeToBuy = function (itemIndex) {
+      items.splice(itemIndex, 1);
+  }
+
 }
 
 })();
